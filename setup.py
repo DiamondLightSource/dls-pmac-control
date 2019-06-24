822e54fb8f1b40a2b9ec31826579bfc016f0fcd7
Giles Knap <giles.knap@diamond.ac.uk>
2019-06-24 16:37:38 +0100

    DLS Python 3 build

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
 from setuptools import setup
 
-# these lines allow the version to be specified in Makefile.private
-import os
-version = os.environ.get("MODULEVER", "0.0")
-
-setup(
-    install_requires = ['dls_pmaclib==1.9.6','numpy==1.13.1'],
-    name = 'dls_pmaccontrol',
-    version = version,
-    description = 'Module',
-    author = 'Ulrik Kofoed Pedersen',
-    author_email = 'Ulrik.Pedersen@diamond.ac.uk',
-    packages = ['dls_pmaccontrol'],
-    entry_points = {'console_scripts': [
-        'dls-pmac-control.py = dls_pmaccontrol.motor:main']},
-    include_package_data = True,
-    package_data = {'': ['*.png']},
-    zip_safe = False
-    )
+setup()
