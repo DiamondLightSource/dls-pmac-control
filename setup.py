0c7039a7c9e78699edadadbf333e3f2bf714bce5
Giles Knap <giles.knap@diamond.ac.uk>
2022-05-06 14:20:47 +0000

    switch to setuptoolsSCM

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-# type: ignore
-import glob
-import importlib.util
-
 from setuptools import setup
 
-# TODO remove this file once versioning with setuptoolsSCM is completed
-
-# Import <package>._version_git.py without importing <package>
-path = glob.glob(__file__.replace("setup.py", "src/*/_version_git.py"))[0]
-spec = importlib.util.spec_from_file_location("_version_git", path)
-vg = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(vg)
-
-setup(cmdclass=vg.get_cmdclass(), version=vg.__version__)
+setup(use_scm_version={"write_to": "src/dls_pmaccontrol/_version.py"})
