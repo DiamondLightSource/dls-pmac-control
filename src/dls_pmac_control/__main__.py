5dd667aa14d9896270238f417592a84ca8db1554
Giles Knap <giles.knap@diamond.ac.uk>
2024-02-11 20:16:19 +0000

    move src to correct folder etc.

--- src/dls_pmac_control/__main__.py
+++ src/dls_pmac_control/__main__.py
@@ -7,10 +7,10 @@ __all__ = ["main"]
 
 def main(args=None):
     parser = ArgumentParser()
-    parser.add_argument("-v", "--version", action="version", version=__version__)
+    parser.add_argument("--version", action="version", version=__version__)
     args = parser.parse_args(args)
 
 
-# test with: python -m dls_pmac_control
+# test with: pipenv run python -m dls_pmaccontrol
 if __name__ == "__main__":
     main()
