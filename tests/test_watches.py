314dde6e4bbd2f5258b19e62e45813a5097fdbc5
Man Ting Chan <man.chan@diamond.ac.uk>
2026-06-25 11:23:47 +0100

    using 'worker'

--- tests/test_watches.py
+++ tests/test_watches.py
@@ -12,7 +12,7 @@ class DummyTestWidget(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.pmac = Mock()
-        self.commsThread = Mock()
+        self.worker = Mock()
 
 
 class WatchesTest(unittest.TestCase):
