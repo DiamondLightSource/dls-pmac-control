0bf3e7dde140e7618065d98f5e2d87d281b1487e
Man Ting Chan <man.chan@diamond.ac.uk>
2026-09-25 14:28:26 +0100

    remove vars

--- tests/test_commsthread.py
+++ tests/test_commsthread.py
@@ -35,8 +35,7 @@ class CommsthreadTest(unittest.TestCase):
         assert self.obj.parent == self.test_widget
         assert self.obj.CSNum == 1
         assert self.obj.gen is None
-        assert self.obj.updateReadyEvent is None
-        assert self.obj.disablePollingStatus is False
+        # assert self.obj.disablePollingStatus is False
         assert self.obj.max_pollrate is None
         assert self.obj.lineNumber == 0
         assert self.obj._watch_window == {}
